0762ebde90f0c03325d3d391131303361c134ff6
David Almandos Irigoyen <davidairigoyen@gmail.com>
2026-09-04 15:28:35 -0300

    feat: configurar base de colores Hex y su relación con Hsl

--- js/script.js
+++ js/script.js
@@ -1,17 +1,57 @@
 let botonConColor = document.getElementsByClassName("btn-generado");
-
 const botonGenerar = document.getElementById("btn-generar");
+const botonHsl = document.getElementById("btn-hsl");
+const botonHex = document.getElementById("btn-hex");
+let caracteresHex = "0123456789ABCDEF";
 
-botonGenerar.addEventListener("click", function() {
-  for (let boton of botonConColor) {
+let modoHsl = false;
+let modoHex = false;
+
+function pintarBotonesHsl () {
+  for (let botonFinalHsl of botonConColor) {
     let hue = Math.floor(Math.random() * 361);
     let sat = Math.floor(Math.random() * 101); 
     let light = Math.floor(Math.random() * 101);
     let color = "hsl(" + hue + ", " + sat + "%, " + light + "%)";
-    boton.style.backgroundColor = color;
-    boton.setAttribute("aria-label", "Color " + color);
+    botonFinalHsl.style.backgroundColor = color;
+    botonFinalHsl.setAttribute("aria-label", "Color " + color);
 }
-});
+};
+
+
+function pintarBotonesHex () {
+  for (let botonFinalHex of botonConColor) {
+    let colorHex = "#";
+    for (let i = 0; i < 6; i++) {
+      let indiceHex = Math.floor(Math.random() * 16);
+      colorHex = colorHex + caracteresHex[indiceHex];
+      }
+    botonFinalHex.style.backgroundColor = colorHex;
+    botonFinalHex.setAttribute("aria-label", "Color " + colorHex);
+  }
+}
+
+botonHsl.addEventListener("click", function() {
+  modoHsl = true;
+  modoHex = false;
+} ) 
+
+botonHex.addEventListener("click", function() {
+  modoHex = true;
+  modoHsl = false;
+})
+
+botonGenerar.addEventListener("click", function() {
+  if (modoHsl) {
+    pintarBotonesHsl();
+    console.log("Pintando en HSL");
+  } else if (modoHex) {
+    pintarBotonesHex();
+    console.log("Pintando en Hex");
+  }
+  }
+);
+
 
 
 
