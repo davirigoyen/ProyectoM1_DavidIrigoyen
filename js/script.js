93eda5df780487d31828f3f75947cd307db38f16
David Irigoyen <davidairigoyen@gmail.com>
2026-09-16 10:48:23 -0300

    "fix: elimino el div toast-bloqueado en html y la const del mismo en script"

--- js/script.js
+++ js/script.js
@@ -17,8 +17,6 @@ botonHex.setAttribute("aria-pressed", false);
 let colorPrimarioClaro = getComputedStyle(document.documentElement).getPropertyValue("--color-primario-claro").trim();
 
 const toast = document.getElementById("toast");
-const toastBloqueado = document.getElementById("toast-bloqueado");
-
 
 function pintarBotonesHsl () {
   for (let botonFinalHsl of botonConColor) {
