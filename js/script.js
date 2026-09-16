8dc34f9bc10d6e92832d00ba650e98b814025d24
David Irigoyen <davidairigoyen@gmail.com>
2026-09-16 13:53:00 -0300

    "feat: crea el array paletasGuardadas"

--- js/script.js
+++ js/script.js
@@ -19,6 +19,7 @@ let colorPrimarioClaro = getComputedStyle(document.documentElement).getPropertyV
 const toast = document.getElementById("toast");
 
 const botonGuardarPaleta = document.querySelector(".btn-guardar-paleta");
+let paletasGuardadas = [];
 
 function pintarBotonesHsl () {
   for (let botonFinalHsl of botonConColor) {
