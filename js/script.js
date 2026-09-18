088718dd9be23918a973ba5638c08dad3b89ccd1
David Irigoyen <davidairigoyen@gmail.com>
2026-09-18 04:17:30 -0300

    "feat: agrega const al botón exportar paleta"

--- js/script.js
+++ js/script.js
@@ -23,6 +23,7 @@ let paletasGuardadas = [];
 const botonPaletas = document.querySelector(".btn-paletas");
 const desplegablePaletas = document.querySelector(".desplegable-paletas");
 const botonCopiarPaleta =document.querySelector(".btn-copiar-paleta");
+const botonExportarPaleta = document.querySelector(".btn-exportar-paleta");
 
 for (let boton of botonConColor) {
 
